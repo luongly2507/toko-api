6749c7ff1480ae61fbf4236dddb4e09b3dcb47b7
luongly2507 <luongly2507@gmail.com>
2023-04-11 12:02:39 +0700

    Create Project

---
+++ toko/src/main/java/com/app/toko/TokoApplication.java
@@ -0,0 +1,13 @@
+package com.app.toko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoApplication.class, args);
+	}
+
+}
---
+++ toko/src/test/java/com/app/toko/TokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.toko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
