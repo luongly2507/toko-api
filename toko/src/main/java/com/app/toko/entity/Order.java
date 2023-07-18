a0f00201e66b11070829f24f48bf51055ee321c8
Lê Khai Trí <lekhaitri.2003@gmail.com>
2023-07-19 06:46:50 +0700

    add receiver name

--- toko/src/main/java/com/app/toko/entity/Order.java
+++ toko/src/main/java/com/app/toko/entity/Order.java
@@ -46,4 +46,8 @@ public class Order {
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private Set<OrderDetail> orderDetails;
+
+    @Column
+    private Double receiverName;
+
 }
